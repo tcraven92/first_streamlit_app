3c70da61cd21c647a58462bc8b631d6e326ce37e
tcraven92 <110599473+tcraven92@users.noreply.github.com>
2022-08-06 16:19:28 +0100

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -44,4 +44,4 @@ streamlit.dataframe(my_data_rows)
 fruit_name = streamlit.text_input('enter ur fruit\'s name', 'banana');
 streamlit.write ('user entered fruit name', fruit_name);
 
-
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
