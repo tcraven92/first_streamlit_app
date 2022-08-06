329cce189a3220a241f4eef40c0aaa053e9508f1
tcraven92 <110599473+tcraven92@users.noreply.github.com>
2022-08-06 16:52:10 +0100

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -60,6 +60,8 @@ if streamlit.button('Get Fruit Load List'):
       my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
       my_data_rows = get_fruit_load_list()
       streamlit.dataframe(my_data_rows)
+
+streamlit.stop()
       
             
                                   
